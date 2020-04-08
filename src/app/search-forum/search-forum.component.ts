ea5395cf0846dc037db0f9d0ae83ed66628f9743
JessDF <jdowding@csumb.edu>
2020-04-08 12:57:26 -0700

    Initial Landing Page Criteria

---
+++ src/app/search-forum/search-forum.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-forum',
+  templateUrl: './search-forum.component.html',
+  styleUrls: ['./search-forum.component.css']
+})
+export class SearchForumComponent {
+
+}
