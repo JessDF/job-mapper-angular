91f114fe13ce3fe796d27d9c9994f2b2cfa82e31
unknown <uantonio@csumb.edu>
2020-04-11 19:45:33 -0700

    adding the Location for Suggestions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SearchForumComponent } from './search-forum/search-forum.component';
+import { AutocompleteComponent } from './autocomplete/autocomplete.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchForumComponent
+    SearchForumComponent,
+    AutocompleteComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    HttpClientModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatTooltipModule,
+    BrowserModule,
+    BrowserAnimationsModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
