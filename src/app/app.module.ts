ada9de2b0e5aee1d753de732d42691c9a7471fa6
unknown <uantonio@csumb.edu>
2020-04-20 17:30:09 -0700

    Nav var and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,15 +12,25 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatButtonModule } from '@angular/material/button'
+import {MatMenuModule} from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AppRoutingModule } from './app-routing.module';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchForumComponent,
-    AutocompleteComponent
+    AutocompleteComponent,
+    AboutComponent,
+    LoginComponent
   ],
   imports: [
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
     CommonModule,
     HttpClientModule,
     MatInputModule,
@@ -30,7 +40,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatAutocompleteModule,
     MatTooltipModule,
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   exports: [],
   providers: [],
