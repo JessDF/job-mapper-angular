09382f36ee2f97cf20b696f440589f701317465a
JessDF <jdowding@csumb.edu>
2020-05-21 19:18:59 -0700

    Initial changes to search functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { SearchForumComponent } from './search-forum/search-forum.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
+import { SearchFunctionalityComponent } from './search-functionality/search-functionality.component'
 
 const routes: Routes = [
   {path: '', component: SearchForumComponent},
   {path: 'About', component: AboutComponent},
-  {path: 'LogIn', component: LoginComponent}
-
+  {path: 'LogIn', component: LoginComponent},
+  {path: 'SearchFunc/:location/:title', component: SearchFunctionalityComponent}
 ]
 
 @NgModule({
